a2c658e4f7243b7d6d9ba18e5967154040e88a2b
pranto0000 <125544914+pranto0000@users.noreply.github.com>
2024-01-26 12:31:09 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const mobile_bars = document.querySelector(".mobile_bars");
+const main_manu = document.querySelector(".main_manu");
+mobile_bars.onclick=function () {
+  main_manu.classList.toggle("open")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// mobile_bars.addEventListener("click", () => {
+//   document.querySelector(".main_manu").classList.toggle("open");
+// });
